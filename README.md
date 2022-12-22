33d65a9f1ae1ca448f13abdcb9b180bfc38c00a1
Kahuds <deividaspa@gmail.com>
2022-12-22 07:12:17 +0000

    Fixed the targeting system and added info to ships to know their location fixed bug where all ship data would be erased.
    Signed-off-by: Kahuds <deividaspa@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # AdaShip
 Ada advanced programming project
+
+
+
+This code shows a battleship game made by Deividas Dapkus for advance progrmaing module part 2.
+
+The Ship class has the following attributes:
+
+length: an integer representing the length of the ship
+name: a string representing the name of the ship
+sunk: a boolean indicating whether the ship has been sunk or not
+row: an integer representing the row of the top left corner of the ship
+col: an integer representing the column of the top left corner of the ship
+direction: a string representing the direction in which the ship is placed on the board, either 'H' for horizontal or 'V' for vertical
+The Board class has the following attributes:
+
+size: an integer representing the size of the board, i.e. the number of rows and columns
+board: a 2D list of strings representing the current state of the board, where each string is either '-' for an unoccupied cell, 'S' for an occupied cell containing a ship, or 'X' for a cell that has been attacked
+ship_board: a 2D list of strings representing the locations of the ships on the board, where each string is either '-' for an unoccupied cell or 'S' for an occupied cell containing a ship
+ships: a list of Ship objects representing the ships on the board
+The Board class has several methods:
+
+print_ship: prints the name and length of each ship on the board
+is_game_over: returns True if all ships on the board have been sunk, and False otherwise
+auto_place_ship: attempts to place a given ship on the board automatically by first trying to place it horizontally, and if that fails, trying to place it vertically
+place_ships: prompts the player to enter the placement for each ship on the board or to place the ship automatically
+print_ships: prints the board with the ships placed on it
+print_board: prints the board with the attacks made on it
+is_valid_move: checks if a given move is a valid attack on the board
+play_move: makes an attack on the board and updates the board and ship states accordingly
+play_game: the main game loop that handles player input, validation, and game updates until the game is over.
--- shipgame.py
+++ shipgame.py
@@ -26,10 +26,13 @@ def get_column(code):
 
 
 class Ship:
-    def __init__(self, length, name):
+    def __init__(self, length, name, row, col, direction):
         self.length = length
         self.name = name
         self.sunk = False
+        self.row = row
+        self.col = col
+        self.direction = direction
 
     def is_sunk(self):
         return self.sunk
@@ -44,15 +47,26 @@ class Board:
     def __init__(self, size, num_ships):
         self.size = size
         self.board = []
+        self.ship_board = []
+
         for i in range(size):
             row = []
             for j in range(size):
                 row.append('-')
             self.board.append(row)
 
+        for i in range(size):
+            row = []
+            for j in range(size):
+                row.append('-')
+            self.ship_board.append(row)
         self.ships = []
         for i in range(num_ships):
-            self.ships.append(Ship(ship_sizes[i], ship_names[i]))
+            self.ships.append(Ship(ship_sizes[i], ship_names[i], 0, 0, 'H'))
+    
+    def print_ship(self):
+        for i in range(len(self.ships)):
+            print(self.ships[i].name, self.ships[i].length)
 
     def is_game_over(self):
         for ship in self.ships:
@@ -60,63 +74,163 @@ class Board:
                 return False
         return True
 
-    def place_ships(self):
+    def auto_place_ship(self, ship):
+        # Try to place the ship horizontally
+        for row in range(self.size):
+            for col in range(self.size - ship.length + 1):
+                valid_placement = True
+                for i in range(ship.length):
+                    if self.ship_board[row][col + i] == 'S':
+                        valid_placement = False
+                        break
+                if valid_placement:
+                    # Place the ship horizontally
+                    for i in range(ship.length):
+                        self.ship_board[row][col + i] = 'S'
+                    ship.row = row
+                    ship.col = col
+                    ship.direction = 'H'
+                    return True
+
+        # If horizontal placement failed, try to place the ship vertically
+        for row in range(self.size - ship.length + 1):
+            for col in range(self.size):
+                valid_placement = True
+                for i in range(ship.length):
+                    if self.board[row + i][col] == 'S':
+                        valid_placement = False
+                        break
+                if valid_placement:
+                    # Place the ship vertically
+                    for i in range(ship.length):
+                        ship.row = row
+                        ship.col = col
+                        self.board[row + i][col] = 'S'
+                    return True
+
+        # If automatic placement failed, return False
+        return False
+
+    def place_ships(self, choice):
         # Place each ship on the board
-        for ship in self.ships:
-            valid_placement = False
-            while not valid_placement:
-                # Print the board and the ship placement prompt
-                self.print_board()
-                print("Enter placement for ship %s (length %d):" % (ship.name, ship.length))
-                row_input = input("Enter row: ")
-                col_input = input("Enter col: ").upper()
+        if choice == 'Player':
+            for ship in self.ships:
+                valid_placement = False
+                while not valid_placement:
+                    # Print the board and the ship placement prompt
+                    self.print_ships()
+                    print("Enter placement for ship %s (length %d):" % (
+                        ship.name, ship.length), "<Enter AUTO to place automatically>")
+                    row_input = input("Enter row: ").upper()
+                    if row_input == 'AUTO':
+                        self.auto_place_ship(ship)
+                        print("Ship placed automatically.")
+                        clear_screen()
+                        break
+                    col_input = input("Enter col: ").upper()
+                    if col_input == 'AUTO':
+                        self.auto_place_ship(ship)
+                        clear_screen()
+                        break
 
-                # Convert the row and column inputs to integer indices
-                row = int(row_input) - 1
-                col = get_column(col_input)
-                orientation = input("Enter orientation (H or V): ").upper()
+                    # Convert the row and column inputs to integer indices
+                    row = int(row_input) - 1
+                    col = get_column(col_input)
+                    orientation = input("Enter orientation (H or V): ").upper()
 
-                # Check if the placement is valid
-                if orientation == 'H':
-                    if col + ship.length > self.size:
-                        print("Invalid placement. Try again.")
-                        continue
-                    for i in range(ship.length):
-                        if self.board[row][col + i] == 'S':
+                    # Check if the placement is valid
+                    if orientation == 'H':
+                        if col + ship.length > self.size:
                             print("Invalid placement. Try again.")
-                            break
-                    else:
-                        # Place the ship on the board
-                        for i in range(ship.length):
-                            self.board[row][col + i] = 'S'
-                        valid_placement = True
-                elif orientation == 'V':
-                    if row + ship.length > self.size:
-                        print("Invalid placement. Try again.")
-                        continue
-                    for i in range(ship.length):
-                        if self.board[row + i][col] == 'S':
-                            print("Invalid placement. Try again.")
-                            break
-                    else:
-                        # Place the ship on the board
-                        for i in range(ship.length):
-                            self.board[row + i][col] = 'S'
-                        valid_placement = True
-                else:
-                    print("Invalid orientation. Try again.")
+                            clear_screen()
 
-                # Clear the screen
-                clear_screen()
+                            continue
+                        for i in range(ship.length):
+                            if self.ship_board[row][col + i] == 'S':
+                                print("Invalid placement. Try again.")
+                                clear_screen()
+
+                                break
+                        else:
+                            # Place the ship on the board
+                            for i in range(ship.length):
+                                self.ship_board[row][col + i] = 'S'
+
+                            valid_placement = True
+                    elif orientation == 'V':
+                        if row + ship.length > self.size:
+                            print("Invalid placement. Try again.")
+                            clear_screen()
+
+                            continue
+                        for i in range(ship.length):
+                            if self.ship_board[row + i][col] == 'S':
+                                print("Invalid placement. Try again.")
+                                clear_screen()
+
+                                break
+                        else:
+                            # Place the ship on the board
+                            for i in range(ship.length):
+                                self.ship_board[row + i][col] = 'S'
+                            valid_placement = True
+                            clear_screen()
+
+                    else:
+                        print("Invalid orientation. Try again.")
+
+                    # Clear the screen
+        elif choice == 'Computer':
+            for ship in self.ships:
+                if not self.auto_place_ship(ship):
+                    print("Error: automatic ship placement failed.")
+                    return False
+        
+    def hide_ships(self):
+        print("SHIPSSS")
+        self.print_ships()
+        self.print_board()
+        print("SHIPSSS2")
+
+        for i in range(self.size):
+            for j in range(self.size):
+                if self.board[i][j] == 'S':
+                    self.board[i][j] = '-'
+        
+        print("WIPED")
+        self.print_ships()
+        self.print_board()
+        print("WIPED2")
 
     def print_ships(self):
-        # Print the column labels
-        print('  ' + ' '.join(chr(ord('A') + i) for i in range(self.size)))
+        colorama.init()
 
-        # Print each row with its corresponding row number and the ship cells
-        for i, row in enumerate(self.board):
-            print(str(i + 1) + ' ' + ' '.join(cell if cell !=
-                  'S' else '#' for cell in row))
+        # Calculate the maximum width of each cell in the board
+        max_width = max(len(str(self.size)) + 1, max(
+            len(str(self.ship_board[i][j])) for i in range(self.size) for j in range(self.size)))
+        # Print the column labels
+        print(' ' * (max_width + 3), end='')
+
+        for i in range(self.size):
+            column_number = i + 1
+            column_label = ""
+
+            while column_number > 0:
+                column_label = chr((column_number - 1) %
+                                   26 + ord('A')) + column_label
+                column_number = (column_number - 1) // 26
+
+            print(colorama.Fore.BLUE + column_label + colorama.Style.RESET_ALL,
+                  end=' ' * (max_width - len(column_label) + 1))
+        print()
+
+        # Print the rows of the board with their corresponding row numbers
+        for i, row in enumerate(self.ship_board):
+            print(colorama.Fore.BLUE + str(i + 1).rjust(max_width) +
+                  colorama.Style.RESET_ALL, end=' ')
+            for j in range(self.size):
+                print(str(row[j]).rjust(max_width), end=' ')
+            print()
 
     def print_board(self):
         colorama.init()
@@ -132,16 +246,17 @@ class Board:
             column_label = ""
 
             while column_number > 0:
-                column_label = chr((column_number - 1) % 26 + ord('A')) + column_label
+                column_label = chr((column_number - 1) %
+                                   26 + ord('A')) + column_label
                 column_number = (column_number - 1) // 26
 
-            print(colorama.Fore.BLUE + column_label + colorama.Style.RESET_ALL,
+            print(colorama.Fore.RED + column_label + colorama.Style.RESET_ALL,
                   end=' ' * (max_width - len(column_label) + 1))
         print()
 
         # Print the rows of the board with their corresponding row numbers
         for i, row in enumerate(self.board):
-            print(colorama.Fore.BLUE + str(i + 1).rjust(max_width) +
+            print(colorama.Fore.RED + str(i + 1).rjust(max_width) +
                   colorama.Style.RESET_ALL, end=' ')
             for j in range(self.size):
                 print(str(row[j]).rjust(max_width), end=' ')
@@ -155,10 +270,20 @@ class Board:
         return self.board[row][col] != 'S'
 
     def handle_move(self, player, other_board):
-        valid_input = False
-        while not valid_input:
-            print("Player %d's turn:" % player)
-            other_board.print_board()
+
+
+        print("Player %d's turn:" % player)
+
+
+        comeback = True
+        
+        
+        while comeback == True:
+
+            self.print_board()
+
+            other_board.print_ships()
+
             row_input = input("Enter row: ")
             col_input = input("Enter col: ").upper()
 
@@ -166,15 +291,42 @@ class Board:
             row = int(row_input) - 1
             col = get_column(col_input)
 
-            if self.is_valid_move(row, col):
-                if other_board.board[row][col] == 'S':
-                    self.board[row][col] = 'X'
-                    other_board.ships[row][col].sink()
+            if other_board.ship_board[row][col] == 'S':
+                self.board[row][col] = 'H'
+                self.check_hit(row, col)
+                if self.is_game_over():
+                    self.print_board()
+                    print("Game over! You won!")
+                    comeback = False
+                    return True  
                 else:
-                    self.board[row][col] = 'O'
-                valid_input = True
+                    self.print_board()
+                    print("Hit!")
+                    comeback = True
+            elif self.board[row][col] == 'H' or self.board[row][col] == 'M':
+                self.print_board()
+                print("You already guessed that location.")
+                comeback = True
             else:
-                print("Invalid move. Try again.")
+                self.board[row][col] = 'M'
+                self.print_board()
+                print("Miss.")
+                comeback = False
+                return False
+
+    def check_hit(self, row, col):
+        for ship in self.ships:
+            if ship.row == row and ship.col == col:
+                ship.sink()
+                return
+            elif ship.direction == 'H':
+                if ship.row == row and col in range(ship.col, ship.col + ship.length):
+                    ship.sink()
+                    return
+            elif ship.direction == 'V':
+                if col == ship.col and row in range(ship.row, ship.row + ship.length):
+                    ship.sink()
+                    return
 
 # define the game loop
 
@@ -184,24 +336,44 @@ def play_game():
     board1 = Board(board_size, len(ship_sizes))
     board2 = Board(board_size, len(ship_sizes))
 
-    board1.place_ships()
-    board2.place_ships()
+    # menu
+
+    print("Welcome to Battleship!")
+    print("1. Player vs Player")
+    print("2. Player vs Computer")
+    print("3. Quit")
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        print("Player vs Player")
+        choice1 = "Player"
+        choice2 = "Player"
+    elif choice == "2":
+        print("Player vs Computer")
+        choice1 = "Player"
+        choice2 = "Computer"
+
+    # place the ships on the boards
+
+    board1.place_ships(choice1)
+    #board1.hide_ships()
+    board2.place_ships(choice2)
+    #board2.hide_ships()
 
     # main game loop
+    print("All ships placed. Let's play!")
     while True:
         # player 1's turn
+
         board1.handle_move(1, board2)
-        board2.print_board()
-        board1.print_ships()  # Print the ships for player 1
+        clear_screen()
         # check if the game is over
         if board1.is_game_over() or board2.is_game_over():
             break
-        clear_screen()
 
         # player 2's turn
+
         board2.handle_move(2, board1)
-        board1.print_board()
-        board2.print_ships()  # Print the ships for player 2
         # check if the game is over
         if board1.is_game_over() or board2.is_game_over():
             break
